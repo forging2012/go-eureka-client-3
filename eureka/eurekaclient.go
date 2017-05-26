648bda47c71e94360ae9ed6a84c82283159b5206
hudangwei <hudangwei@huimin100.cn>
2017-05-26 10:04:24 +0800

    添加heartbeat

--- eureka/eurekaclient.go
+++ eureka/eurekaclient.go
@@ -1,12 +1,42 @@
 package eureka
 
 import (
-	"time"
 	"encoding/json"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 )
 
-func Register(eurekaServerUrl,appName,port,securePort,instanceId,ipAddress string) {
+func handleSigterm(eurekaServerUrl, appName, ipAddress string) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	go func() {
+		<-c
+		UnRegister(eurekaServerUrl, appName, ipAddress)
+		os.Exit(1)
+	}()
+}
+
+func startHeartbeat(eurekaServerUrl, appName, ipAddress string) {
+	for {
+		time.Sleep(time.Second * 30)
+		heartbeat(eurekaServerUrl, appName, ipAddress)
+	}
+}
+
+func heartbeat(eurekaServerUrl, appName, ipAddress string) {
+	heartbeatAction := HttpAction{
+		Url:         eurekaServerUrl + "/eureka/apps/" + appName + "/" + ipAddress,
+		Method:      "PUT",
+		ContentType: "application/json;charset=UTF-8",
+	}
+	doHttpRequest(heartbeatAction)
+}
+
+func Register(eurekaServerUrl, appName, port, securePort, instanceId, ipAddress string) {
 	tpl := string(Template)
 	tpl = strings.Replace(tpl, "${ipAddress}", ipAddress, -1)
 	tpl = strings.Replace(tpl, "${port}", port, -1)
@@ -20,10 +50,13 @@ func Register(eurekaServerUrl,appName,port,securePort,instanceId,ipAddress strin
 		ContentType: "application/json;charset=UTF-8",
 		Body:        tpl,
 	}
+
 	var result bool
 	for {
 		result = doHttpRequest(registerAction)
 		if result {
+			handleSigterm(eurekaServerUrl, appName, ipAddress)
+			go startHeartbeat(eurekaServerUrl, appName, ipAddress)
 			break
 		} else {
 			time.Sleep(time.Second * 5)
@@ -31,16 +64,7 @@ func Register(eurekaServerUrl,appName,port,securePort,instanceId,ipAddress strin
 	}
 }
 
-func Heartbeat(eurekaServerUrl,appName,ipAddress string) {
-	heartbeatAction := HttpAction{
-		Url:         eurekaServerUrl + "/eureka/apps/" + appName + "/" + ipAddress,
-		Method:      "PUT",
-		ContentType: "application/json;charset=UTF-8",
-	}
-	doHttpRequest(heartbeatAction)
-}
-
-func UnRegister(eurekaServerUrl,appName,ipAddress string) {
+func UnRegister(eurekaServerUrl, appName, ipAddress string) {
 	unRegisterAction := HttpAction{
 		Url:         eurekaServerUrl + "/eureka/apps/" + appName + "/" + ipAddress,
 		ContentType: "application/json;charset=UTF-8",
@@ -69,7 +93,7 @@ func GetServices(eurekaServerUrl string) ([]EurekaApplication, error) {
 	}
 }
 
-func GetServiceInstances(eurekaServerUrl,appName string) ([]EurekaInstance, error) {
+func GetServiceInstances(eurekaServerUrl, appName string) ([]EurekaInstance, error) {
 	var m EurekaServiceResponse
 	queryAction := HttpAction{
 		Url:         eurekaServerUrl + "/eureka/apps/" + appName,
